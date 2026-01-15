04e1de357fd8541963f007dd1b732bc14b7182eb
satyajeetpatra1 <satyajeetpatra.dev@gmail.com>
2026-01-15 09:24:56 +0530

    a simple boiler plate code added

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello from yt_clone_be_node");
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
